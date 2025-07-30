c829780a7cdf95139c30046acbc8f13312d8d2fe
kiwianatra <mcshonax@gmail.com>
2025-07-30 14:22:28 +0300

    #1

---
+++ public/UI/main.kt
@@ -0,0 +1,80 @@
+package com.elemsocial.designsystem.components
+
+// Base Components
+object Avatar
+object Notifications
+object Cover
+object MusicCover
+object ContextMenu
+object Gift
+object SavesAvatar
+
+// Buttons
+object AddButton
+object FormButton
+object SendButton
+object BoxButton
+object BackButton
+object Button
+object Switch
+object GovernButtons
+object GitHubButton
+object ListButtons
+
+// Inputs
+object Textarea
+object TextInput
+object CopyInput
+object SocialInput
+object AvatarInput
+object CoverInput
+
+// Layout
+object NavigatedHeader
+object BoxButtons
+object QuestionModal
+object RoutedModal
+object AlertModal
+object Block
+object PartitionName
+
+// Tabs
+object MaterialTabs
+object Tabs
+
+// Pickers
+object EmojiPicker
+object DropdownSelect
+
+// Text
+object CopyTextBox
+object ApiCode
+object Name
+object Text
+
+// Player
+object Slider
+object Video
+
+// Loaders
+object DownloadProgress
+object ProgressRing
+
+// Simple Elements
+object User
+
+// Sidebar Components (exported separately as in original)
+object Sidebar
+object MobileSidebarMenu
+object MenuItem
+object UserCard
+object ChannelItem
+object AccountsList
+
+// Type definitions (using Kotlin interfaces/classes)
+interface AvatarProps {
+    val src: String
+    val size: Int
+    val alt: String
+    // Add other avatar properties as needed
+}
